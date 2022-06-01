da533e6b8f4cd3570db6f3fd0cbe7e06def82e3f
unknown <aseuo0726@gmail.com>
2022-06-01 21:31:35 +0800

    init

--- src/solution/p0035_search_insert_position/mod.rs
+++ src/solution/p0035_search_insert_position/mod.rs
@@ -1,11 +1,11 @@
-pub struct Solution;
+pub struct Solution {}
 
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        let (mut left, mut right, mut mid) = (0, nums.len() - 1, 0);
+        let (mut left, mut right) = (0, nums.len() - 1);
 
         while left <= right {
-            mid = left + (right - left) / 2;
+            let mid = left + (right - left) / 2;
             if nums[mid] == target {
                 return mid as i32;
             } else if nums[mid] < target {
