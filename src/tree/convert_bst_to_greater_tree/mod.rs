7c00e12a6422ecf2febdb471d9bb1806b3e66584
izumijuro <aseuo0726@gmail.com>
2022-12-08 16:32:16 +0800

    :sparkles: solve [538]

---
+++ src/tree/convert_bst_to_greater_tree/mod.rs
@@ -0,0 +1,22 @@
+use crate::utils::tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+struct Solution;
+type T = Option<Rc<RefCell<TreeNode>>>;
+
+impl Solution{
+    pub fn convert_bst(root: T) -> T {
+        fn traversal(root: &T, sum: &mut i32) {
+            if let Some(node) = root {
+                traversal(&node.borrow().right, sum);
+                *sum += node.borrow().val;
+                node.borrow_mut().val = *sum;
+                traversal(&node.borrow().left, sum);
+            }
+        }
+        let mut sum = 0;
+        traversal(&root, &mut sum);
+        root
+    }
+}
