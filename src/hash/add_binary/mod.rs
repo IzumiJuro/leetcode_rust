9280fedc1af8dec2637ee68a77056b4a6e3c42ab
izumijuro <aseuo0726@gmail.com>
2022-12-08 23:28:09 +0800

    :sparkles: solve [67]

---
+++ src/hash/add_binary/mod.rs
@@ -0,0 +1,30 @@
+struct Solution;
+
+impl Solution {
+    pub fn add_binary(a: String, b: String) -> String {
+        let mut vec_a = a.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>();
+        let mut vec_b = b.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>();
+
+        let len = vec_a.len().max(vec_b.len());
+        let mut carry = 0;
+        let mut res = Vec::new();
+        
+        (0..len).for_each(|_| {
+            let a = vec_a.pop().unwrap_or(0);
+            let b = vec_b.pop().unwrap_or(0);
+            let sum = a + b + carry;
+            carry = 0;
+            if sum > 1 {
+                carry = 1;
+                res.push(sum - 2);
+            } else {
+                res.push(sum);
+            }
+        });
+        if carry == 1 {
+            res.push(1);
+        }
+
+        res.iter().map(|i| i.to_string()).rev().collect()
+    }
+}
